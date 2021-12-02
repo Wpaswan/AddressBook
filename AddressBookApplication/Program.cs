cdc2fc3f94797ba1c113ea63ca53f2eedb750885
Wpaswan <dablumcapaswan@gmail.com>
2021-12-02 10:10:28 -0800

    [Dablu,modified the codes to Contact creation]

--- AddressBookApplication/Program.cs
+++ AddressBookApplication/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Welcome to Address Book Programm!");
+﻿public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string[] Addresses { get; set; }
+    public int zip { get; set; }
+    public string email { get; set; }
+    public static List<Person> People = new List<Person>();
+}
